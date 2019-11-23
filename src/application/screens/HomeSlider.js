6a36fbb14cb7cec426c426ed44b3f28783121e5e
Pedro Melo <pedromelo@MBPRO01-UNASUS.local>
2019-11-23 17:33:23 -0300

    dev

--- src/application/screens/HomeSlider.js
+++ src/application/screens/HomeSlider.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Carousel, Container } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 
+import '../styles/Carousel.css' 
 
 export default function HomeSlider() {
 
@@ -8,7 +9,7 @@ export default function HomeSlider() {
             <Carousel>
                 <Carousel.Item>
                     <img
-                        className=""
+                        className="Img-Carousel"
                         src="https://www.proteste.org.br/-/media/proteste/images/home/seguros/seguro-vida-cobertura.jpg?rev=58118673-a4cc-4d43-97de-b154d9f76baf"
                         alt="First slide"
                     />
@@ -17,7 +18,7 @@ export default function HomeSlider() {
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className=""
+                        className="Img-Carousel"
                         src="https://jurosbaixos.com.br/conteudo/wp-content/uploads/sites/2/2018/02/media-20180205.png"
                         alt="Third slide"
                     />
@@ -27,7 +28,7 @@ export default function HomeSlider() {
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className=""
+                        className="Img-Carousel"
                         src="https://www.pattini.com.br/wp-content/uploads/2015/12/o-que-o-seguro-residencial-pode-e-deve-cobrir-1078x539.jpg"
                         alt="Third slide"
                     />
