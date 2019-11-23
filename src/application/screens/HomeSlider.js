7f3f711958c1b1c37e662fc5c0d24a1de18c5670
Pedro Melo <pedromelo@MBPRO01-UNASUS.local>
2019-11-23 19:17:33 -0300

    dev

--- src/application/screens/HomeSlider.js
+++ src/application/screens/HomeSlider.js
@@ -1,41 +1,34 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import { isMobile } from 'react-device-detect'
 
-import '../styles/Carousel.css' 
+import '../styles/Carousel.css'
 
 export default function HomeSlider() {
 
     return (
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="Img-Carousel"
-                        src={require('../images/carro.jpg')}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="Img-Carousel"
-                        src="https://jurosbaixos.com.br/conteudo/wp-content/uploads/sites/2/2018/02/media-20180205.png"
-                        alt="Third slide"
-                    />
+        <Carousel>
+            {/*<Carousel.Item>
+                <img
+                    carousel={isMobile ? "mobile" : "browser"}
+                    src={require('../images/carro.jpg')}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                </Carousel.Caption>
+            </Carousel.Item>*/}
+            
+            <Carousel.Item>
+                <img
+                    carousel={isMobile ? "mobile" : "browser"}
+                    className="Img-Carousel"
+                    src={require('../images/shakinghands.jpg')}
+                    alt="Third slide"
+                />
 
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="Img-Carousel"
-                        src="https://www.pattini.com.br/wp-content/uploads/2015/12/o-que-o-seguro-residencial-pode-e-deve-cobrir-1078x539.jpg"
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+                <Carousel.Caption>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 }
