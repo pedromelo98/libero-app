10312717f0da02a910c70e0747d009c5e6ce130c
Pedro Melo <pedromelo@MBPRO01-UNASUS.local>
2019-11-23 16:53:27 -0300

    dev

--- src/application/components/LiberoBottomBar.js
+++ src/application/components/LiberoBottomBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../App.css'
+import '../styles/Footer.css'
 import { Icon } from 'semantic-ui-react'
 
 class LiberoBottomBar extends React.PureComponent {
--- src/application/components/SocialMedia.js
+++ src/application/components/SocialMedia.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../App.css';
+import '../styles/SocialMedia.css';
 import { Icon } from 'semantic-ui-react';
 
 class SocialMedia extends React.PureComponent {
