326f04d7c8e4bd22af7c68baf13b616a9133a663
Pedro Melo <pedromelo@MBPRO01-UNASUS.local>
2019-11-23 21:00:23 -0300

    dev

--- src/application/components/LiberoBottomBar.js
+++ src/application/components/LiberoBottomBar.js
@@ -5,26 +5,33 @@ import { withRouter } from 'react-router-dom';
 
 class LiberoBottomBar extends React.PureComponent {
 
+    setSelectedByPath(path) {
+        if (this.props.location && path === this.props.location.pathname) {
+            return 'Bottom-selected'
+        }
+        return 'Bottom-item'
+    }
+
     render() {
         return (
             <div className="Bottom-bar" >
                 <div conteudo="bottom" >
-                   <a href="/insurance" className="Bottom-item" >
-                       <Icon size='big' name='shield' />
-                       <p className='Bottom-text' >Seguros</p>
-                   </a>
-                   <a href="/" className="Bottom-item" >
-                       <Icon size='big' name='home' />
-                       <p className='Bottom-text' >Home</p>
-                   </a>
-                   <a href="/social" className="Bottom-item" >
-                       <Icon size='big' name='whatsapp' />
-                       <p className='Bottom-text' >Social</p>
-                   </a>
-                   <a href="" className="Bottom-item" >
-                       <Icon size='big' name='phone' />
-                       <p className='Bottom-text' >Contato</p>
-                   </a>
+                    <a href="/insurance" className={this.setSelectedByPath('/insurance')} >
+                        <Icon size='big' name='shield' />
+                        <p className='Bottom-text' >Seguros</p>
+                    </a>
+                    <a href="/" className={this.setSelectedByPath('/')} >
+                        <Icon size='big' name='home' />
+                        <p className='Bottom-text' >Home</p>
+                    </a>
+                    <a href="/social" className={this.setSelectedByPath('/social')} >
+                        <Icon size='big' name='whatsapp' />
+                        <p className='Bottom-text' >Social</p>
+                    </a>
+                    <a href="" className={this.setSelectedByPath('/contact')} >
+                        <Icon size='big' name='phone' />
+                        <p className='Bottom-text' >Contato</p>
+                    </a>
                 </div>
             </div>
 
@@ -32,4 +39,4 @@ class LiberoBottomBar extends React.PureComponent {
     }
 }
 
-export default  withRouter(LiberoBottomBar);
+export default withRouter(LiberoBottomBar);
