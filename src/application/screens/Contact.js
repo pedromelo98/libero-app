bee3f120139c190dc44d8b3f5fd0d7f074196ea5
Pedro Melo <pedromelo@MBPRO01-UNASUS.local>
2019-12-05 21:34:45 -0300

    dev

--- src/application/screens/Contact.js
+++ src/application/screens/Contact.js
@@ -1,37 +1,57 @@
 import React from 'react';
-import '../styles/Social.css';
-import './Screens.css';
+import '../styles/Contact.css';
 import { Icon } from 'semantic-ui-react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/styles';
-import { LINK_LIGACAO } from '../constants/Links'
-
-const useStyles = makeStyles(theme => ({
-    btn: {
-        marginTop: 20
-    }
-}));
+import { isMobile } from 'react-device-detect';
 
 
-function Contact() {
-    const classes = useStyles();
+class Contact extends React.Component {
 
 
-    return (
-        <div className="Social" >
-            <p className="Intro-font" >Entre em contato conosco:</p>
-            <div className="Socials" >
-                <div className="Social-item" >
-                    <Icon name='phone' size='huge' />
-                    <p className="Social-font" >(34) 3322-6000</p>
+
+    render() {
+        if (true) {
+            return (
+                <div className="Contact" >
+                    <p className="Intro-font" >Entre em contato conosco:</p>
+                    <div className="Container" >
+                        <div className="Contacts" >
+                            <div className="Contact-item" >
+                                <Icon name='phone' size='big' />
+                                <p className="Contact-font" >(34) 3322-6000</p>
+                            </div>
+                            <div className="Contact-item" >
+                                <Icon name='envelope' size='big' />
+                                <p className="Contact-font" >libero@liberocorretora.com.br</p>
+                            </div>
+                        </div>
+
+                        <div className="Contacts" >
+                            
+                            <div className="Contact-item" >
+                                <p className="Contact-font" >Uberaba - MG, R. Padre Zeferino, 542</p>
+                            </div>
+                            <div className="Contact-item" >
+                                <p className="Contact-font" >Bairro Estados Unidos</p>
+                            </div>
+                            <div className="Contact-item" >
+                                <p className="Contact-font" >CEP: 38015-160</p>
+                            </div>
+                        </div>
+
+                        <a href="https://maps.google.com/?q=LÍBERO CORRETORA DE SEGUROS" className="Img-location" >
+                            <div className="Overlay-Button" >
+                                <p className="Button-Text" >Visualizar no mapa</p>
+                            </div>
+                            <img className="Libero-Location" src={require("../images/liberoloc.png")} />
+                        </a>
+
+
+                    </div>
                 </div>
+            )
+        }
+    }
 
-                <Button href={LINK_LIGACAO} className={classes.btn} color="primary" variant="contained">
-                    Solicite uma Ligação!
-                </Button>
-            </div>
-        </div>
-    );
 
 }
 
