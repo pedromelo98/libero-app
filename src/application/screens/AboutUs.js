fcb409d09491f414d48a353bbecf7f3454bfab2f
Pedro Martins Melo <pedromelo@seimac03.local>
2019-12-09 15:14:51 -0300

    dev

--- src/application/screens/AboutUs.js
+++ src/application/screens/AboutUs.js
@@ -3,11 +3,81 @@ import './Screens.css';
 import '../styles/About.css';
 import 'semantic-ui-css/semantic.min.css';
 
-import { Grid, Label, Segment } from 'semantic-ui-react'
+import { Grid, Label, Segment, Icon } from 'semantic-ui-react'
+import { isMobile } from 'react-device-detect';
 
 class AboutUs extends React.PureComponent {
 
     render() {
+        if (isMobile) {
+            return (
+                <div className="About-Us" >
+                    <text className="Intro-font" >Sobre a Líbero:</text>
+                    <div className="Component-Mobile" >
+                        <Segment className="Cards" color="green" raised>
+                            <Label color='green' ribbon>
+                                <p className="TitleFeatures" >
+                                    <Icon name="handshake" size="large" />
+                                    MISSÃO
+                                </p>
+                            </Label>
+                            <p className="Text-Component" >
+                                Oferecer uma assessoria de qualidade com as melhores soluções em seguros, de acordo com o perfil de cada cliente, com o objetivo de garantir a tranquilidade e confiança.
+                            </p>
+
+                        </Segment>
+                        <Segment className="Cards" color="green" raised>
+                            <Label color='green' ribbon>
+                                <p className="TitleFeatures" >
+                                    <Icon name="eye" size="large" />
+                                    VISÃO
+                                </p>
+                            </Label>
+                            <p className="Text-Component" >
+                                Ser referência na corretagem de seguros, consolidando-se na liderança regional, mantendo a qualidade e proximidade com nossos clientes.
+                                </p>
+
+                        </Segment>
+                        <Segment color="green" className="Cards" raised>
+                            <Label color='green' ribbon>
+                                <p className="TitleFeatures" >
+                                    <Icon name="check circle" size="large" />
+                                    VALORES
+                                </p>
+                            </Label>
+                            <p className="Text-Component" >
+                                Ética, confiança, credibilidade e transparência.
+                            </p>
+                        </Segment>
+
+                        <Segment color="green" className="Cards" raised>
+                            <Label color='green' ribbon>
+                                <img style={{ paddingBottom: 5, paddingRight: 5 }} src={require("../images/liberoicon.png")} />
+                                QUEM SOMOS
+                            </Label>
+                            <p className="Text-Component" >
+                                Somos uma corretora de seguros comprometida com a qualidade e agilidade
+                                nos processos, alem de se consolidar a cada ano pelo atendimento consultivo
+                                atrelado a um preço justo.
+                                a Líbero seguros objetiva prestar uma assessoria personalizada de acordo com
+                                as necessidades daqueles que buscam custo-benefício e qualidade.
+                                a empresa foi fundada pelo sócio-proprietário Tiago Souto, que atua há 15 anos
+                                no mercado financeiro, destacando-se em grandes instituições como Banco
+                                Bradesco, Santander e HSBC. É graduado em administração de empresas(FCETM) e
+                                pós graduado com MBA em gestão de pessoas(FGV). Possui as certificações CPA10 e
+                                CPA20(ANBIMA), alem da habilitação como corretor de seguros pleno de todos os
+                                ramos(SUSEP).
+                                Nesses anos de mercado, a ascensão e o sucesso da empresa se da pela eficiência
+                                de uma equipe capacitada, que atua de forma segmentada, além do trabalho
+                                incansável de seu fundador, oferecendo, com confiança, as melhores soluções
+                                em seguros.
+                            </p>
+                        </Segment>
+
+                    </div>
+                </div>
+            )
+        }
         return (
             <div className="About-Us" >
                 <text className="Intro-font" >Sobre a Líbero:</text>
@@ -16,7 +86,10 @@ class AboutUs extends React.PureComponent {
                         <Grid.Column>
                             <Segment className="Cards" color="green" raised>
                                 <Label color='green' ribbon>
-                                    MISSÃO
+                                    <p className="TitleFeatures" >
+                                        <Icon name="handshake" size="big" />
+                                        MISSÃO
+                                    </p>
                                 </Label>
                                 <p className="Text-Component" >
                                     Oferecer uma assessoria de qualidade com as melhores soluções em seguros, de acordo com o perfil de cada cliente, com o objetivo de garantir a tranquilidade e confiança.
@@ -27,7 +100,10 @@ class AboutUs extends React.PureComponent {
                         <Grid.Column>
                             <Segment className="Cards" color="green" raised>
                                 <Label color='green' ribbon>
-                                    VISÃO
+                                    <p className="TitleFeatures" >
+                                        <Icon name="eye" size="big" />
+                                        VISÃO
+                                    </p>
                                 </Label>
                                 <p className="Text-Component" >
                                     Ser referência na corretagem de seguros, consolidando-se na liderança regional, mantendo a qualidade e proximidade com nossos clientes.
@@ -38,7 +114,10 @@ class AboutUs extends React.PureComponent {
                         <div className="Values" >
                             <Segment color="green" className="Segment Cards" raised>
                                 <Label color='green' ribbon>
-                                    VALORES
+                                    <p className="TitleFeatures" >
+                                        <Icon name="check circle" size="big" />
+                                        VALORES
+                                    </p>
                                 </Label>
                                 <p className="Text-Component" >
                                     Ética, confiança, credibilidade e transparência.
--- src/application/screens/Contact.js
+++ src/application/screens/Contact.js
@@ -9,13 +9,12 @@ class Contact extends React.Component {
 
 
     render() {
-        if (true) {
+        if (isMobile) {
             return (
                 <div className="Contact" >
                     <p className="Intro-font" >Entre em contato conosco:</p>
-                    <div className="Container" >
-
-                        <div align="right" className="Contacts" >
+                    <div className="Container-Mobile" >
+                        <a href="https://maps.google.com/?q=LÍBERO CORRETORA DE SEGUROS - Rua Padre Zeferino - Estados Unidos, Uberaba - MG" align="right" className="Contacts" >
                             <Icon name='map marker alternate' size='big' />
                             <div className="Contact-item" >
                                 <p className="Contact-font" >Uberaba - MG, R. Padre Zeferino, 542</p>
@@ -26,13 +25,6 @@ class Contact extends React.Component {
                             <div className="Contact-item" >
                                 <p className="Contact-font" >CEP: 38015-160</p>
                             </div>
-                        </div>
-
-                        <a href="https://maps.google.com/?q=LÍBERO CORRETORA DE SEGUROS - Rua Padre Zeferino - Estados Unidos, Uberaba - MG" className="Img-location" >
-                            <div className="Overlay-Button" >
-                                <p className="Button-Text" >Visualizar no mapa</p>
-                            </div>
-                            <img className="Libero-Location" src={require("../images/liberoloc.png")} />
                         </a>
 
                         <div className="Contacts" >
@@ -50,6 +42,45 @@ class Contact extends React.Component {
                 </div>
             )
         }
+        return (
+            <div className="Contact" >
+                <p className="Intro-font" >Entre em contato conosco:</p>
+                <div className="Container" >
+
+                    <div align="right" className="Contacts" >
+                        <Icon name='map marker alternate' size='big' />
+                        <div className="Contact-item" >
+                            <p className="Contact-font" >Uberaba - MG, R. Padre Zeferino, 542</p>
+                        </div>
+                        <div className="Contact-item" >
+                            <p className="Contact-font" >Bairro Estados Unidos</p>
+                        </div>
+                        <div className="Contact-item" >
+                            <p className="Contact-font" >CEP: 38015-160</p>
+                        </div>
+                    </div>
+
+                    <a href="https://maps.google.com/?q=LÍBERO CORRETORA DE SEGUROS - Rua Padre Zeferino - Estados Unidos, Uberaba - MG" className="Img-location" >
+                        <div className="Overlay-Button" >
+                            <p className="Button-Text" >Visualizar no mapa</p>
+                        </div>
+                        <img className="Libero-Location" src={require("../images/liberoloc.png")} />
+                    </a>
+
+                    <div className="Contacts" >
+                        <div align="left" className="Contact-item" >
+                            <Icon name='phone' size='big' />
+                            <p className="Contact-font" >(34) 3322-6000</p>
+                        </div>
+                        <div align="left" className="Contact-item" >
+                            <Icon name='envelope' size='big' />
+                            <p className="Contact-font" >libero@liberocorretora.com.br</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        )
     }
 
 
